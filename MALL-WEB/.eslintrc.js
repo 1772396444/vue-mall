b126618de36b13325e4051213a67947188797497
龙龙🙄 <1772396444@qq.com>
2020-09-23 18:41:13 +0800

    eslint样式键定义

--- MALL-WEB/.eslintrc.js
+++ MALL-WEB/.eslintrc.js
@@ -1,29 +1,33 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+	root: true,
+	parserOptions: {
+		parser: 'babel-eslint'
+	},
+	env: {
+		es6: true,
+		node: true,
+		browser: true,
+	},
+	extends: [
+		"plugin:vue/essential",
+		"standard"
+	],
+	plugins: [
+		'vue'
+	],
+	rules: {
+		"eol-last": [0],
+		// 缩进严格,一个tab四个空格
+		"indent": [2, 4],
+		// 严格对比,判断相等必须用 === !==
+		"eqeqeq": [2, 'always'],
+		"semi": [2, 'never'],
+		// 不允许new对象之后不使用
+		"no-new": [0],
+		'generator-star-spacing': 'off',
+		// 测试环境不允许使用 debugger
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+	}
 }
---
+++ MALL-WEB/src/store/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+	state: {
+		token: '',
+		routes: [],
+		buttons: [],
+	},
+	mutations: {
+		token(state, token) {
+			state.token = token
+			localStorage['token'] = token
+		},
+		routes(state, routes) {
+			state.routes = routes
+			localStorage['routes'] = JSON.stringify(routes)
+		},
+		buttons(state, buttons) {
+			state.buttons = buttons
+			localStorage['buttons'] = JSON.stringify(buttons)
+		}
+	},
+	getters: {
+		token: state => {
+			if (state.token === '') {
+				state.token = localStorage.getItem('token')
+			}
+			return state.token
+		},
+		routes: state => {
+			if (state.routes.length === 0) {
+				state.routes = JSON.parse(localStorage.getItem('routes'))
+			}
+			return state.routes
+		},
+		buttons: state => {
+			if (state.buttons.length === 0) {
+				state.buttons = JSON.parse(localStorage.getItem('buttons'))
+			}
+			return state.buttons
+		}
+	},
+	actions: {},
+	modules: {}
+});
